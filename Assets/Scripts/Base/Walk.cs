0c9eebbb7e2a106c9141545d0a858faaacafda1b
ChloePrime <chloe_von_einzbern@qq.com>
2020-10-09 17:32:34 +0800

    添加行走物体相撞转向功能，修改刚体属性
    Signed-off-by: ChloePrime <chloe_von_einzbern@qq.com>

--- Assets/Scripts/Base/Walk.cs
+++ Assets/Scripts/Base/Walk.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,18 @@ namespace SweetMoleHouse.MarioForever.Base
 {
     /// <summary>
     /// 行走的东西
+    /// 类似于RE的组1
     /// </summary>
     public class Walk : BasePhysics
     {
         [Header("物品行走设置")]
         [SerializeField, RenameInInspector("行走速度")]
         protected float walkSpeed = 1.5f;
+        [SerializeField, RenameInInspector("与其他行走物品相撞")]
+        private bool collideWithOthers = true;
+        /// <summary>
+        /// 带符号的行走速度
+        /// </summary>
         protected float realWalkSpeed;
 
         protected override void Start()
@@ -29,6 +36,28 @@ namespace SweetMoleHouse.MarioForever.Base
         {
             base.OnHitWallX(colliders);
             realWalkSpeed *= -1;
+        }
+
+        protected virtual void OnCollisionEnter2D(Collision2D collision)
+        {
+            var that = collision.collider.GetComponent<Walk>();
+            if (that == null)
+            {
+                return;
+            }
+            //如果任意一方设置了不与其他物体碰撞则忽略
+            if (!(collideWithOthers && that.collideWithOthers))
+            {
+                return;
+            }
+            if (transform.position.x < that.transform.position.x)
+            {
+                realWalkSpeed = -walkSpeed;
+            }
+            else
+            {
+                realWalkSpeed = walkSpeed;
+            }
         }
     }
 }
