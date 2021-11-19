3120157beabd7dfa2aa1c085b014f39110dcf997
ChloePrime <chloe_von_einzbern@qq.com>
2021-11-19 16:12:41 +0800

    bugfix in AssetNaming.cs

--- Assets/MarioForever/Scripts/Editor/AssetNaming.cs
+++ Assets/MarioForever/Scripts/Editor/AssetNaming.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEngine;
 
 namespace SweetMoleHouse.MarioForever.Scripts.Editor {
 /// <summary>
@@ -14,39 +15,43 @@ namespace SweetMoleHouse.MarioForever.Scripts.Editor {
 public static class AssetNaming {
     [MenuItem("MarioForever/规范资源命名")]
     public static void RegulateAssetNaming() {
-        const string root = "./Assets/MarioForever/";
+        const string root = "Assets/MarioForever/";
 
-        foreach (var file in
+        foreach (string assetPath in
             Directory.EnumerateFiles(root, "*.*", SearchOption.AllDirectories)
         ) {
-            if (!File.Exists(file) || file.EndsWithAny(".meta", ".cs", ".asset")) {
+            if (!File.Exists(assetPath) || assetPath.EndsWithAny(".meta", ".cs", ".asset")) {
                 continue;
             }
 
-            string assetPath = Path.GetRelativePath(root, file);
-            string assetName = Path.GetFileName(file);
+            string assetName = Path.GetFileName(assetPath);
             string dir = assetPath[..^assetName.Length];
 
             string newAssetName;
-            if (file.EndsWithAny(".png", ".jpg") || !assetName.StartsWith("T_")) {
+            if (assetPath.EndsWithAny(".png", ".jpg") || !assetName.StartsWith("T_")) {
                 newAssetName = "T_" + assetName;
-            } else if (file.EndsWith(".anim")       || !assetName.StartsWith("An_")) {
+            } else if (assetPath.EndsWith(".anim")       || !assetName.StartsWith("An_")) {
                 newAssetName = "An_" + assetName;
-            } else if (file.EndsWith(".controller") || !assetName.StartsWith("Asm_")) {
+            } else if (assetPath.EndsWith(".controller") || !assetName.StartsWith("Asm_")) {
                 newAssetName = "Asm_" + assetName;
-            } else if (file.EndsWith(".mat")        || !assetName.StartsWith("M_")) {
+            } else if (assetPath.EndsWith(".mat")        || !assetName.StartsWith("M_")) {
                 newAssetName = "M_" + assetName;
-            } else if (file.EndsWith(".shader")     || !assetName.StartsWith("S_")) {
+            } else if (assetPath.EndsWith(".shader")     || !assetName.StartsWith("S_")) {
                 newAssetName = "S_" + assetName;
             } else {
+                Debug.Log($"Skip asset {assetPath} -> ");
                 continue;
             }
 
             newAssetName = newAssetName.Replace(" ", "");
             if (assetName != newAssetName) {
-                AssetDatabase.RenameAsset(assetPath, dir + newAssetName);
+                string newAssetPath = dir + newAssetName;
+                AssetDatabase.RenameAsset(assetPath, newAssetPath);
+                Debug.Log($"Rename asset {assetPath} -> {newAssetPath}");
             }
         }
+        
+        AssetDatabase.Refresh();
     }
 
     private static bool EndsWithAny(this string self, params string[] suffixes) {
