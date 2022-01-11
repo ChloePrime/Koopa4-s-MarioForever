8856829be705f440a77f2401564e2a644ddee7d3
ChloePrime <chloe_von_einzbern@qq.com>
2022-01-11 11:00:33 +0800

    金币

---
+++ Assets/MarioForever/Scripts/Bonus/Coin.cs
@@ -0,0 +1,16 @@
+using SweetMoleHouse.MarioForever.Scripts.Persistent;
+using SweetMoleHouse.MarioForever.Scripts.Player;
+using SweetMoleHouse.MarioForever.Scripts.Util;
+using UnityEngine;
+
+namespace SweetMoleHouse.MarioForever.Scripts.Bonus {
+public class Coin : MonoBehaviour {
+    [SerializeField] private int faceValue = 1;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.GetHost().TryGetComponent(out Mario mario)) {
+            MarioProperty.AddCoin(faceValue);
+            Destroy(gameObject);
+        }
+    }
+}
+}
