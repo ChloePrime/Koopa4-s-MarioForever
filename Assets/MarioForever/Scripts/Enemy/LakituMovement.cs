27ee63c0c89527b507fd2d8190a7af617605b289
ChloePrime <chloe_von_einzbern@qq.com>
2022-03-28 14:19:15 +0800

    刺猬云移动

---
+++ Assets/MarioForever/Scripts/Enemy/LakituMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using SweetMoleHouse.MarioForever.Scripts.Player;
+using UnityEngine;
+
+namespace SweetMoleHouse.MarioForever.Scripts.Enemy {
+public class LakituMovement : MonoBehaviour {
+    [SerializeField] private float accNear = 78.125f;
+    [SerializeField] private float accFar = 15.625f;
+    /// <summary>
+    /// ±9 in CTF
+    /// </summary>
+    [SerializeField] private float maxSpeed = 14f;
+    /// <summary>
+    /// ±2 in CTF
+    /// </summary>
+    [SerializeField] private float lowSpeed = 3.125f;
+
+    private void FixedUpdate() {
+        Mario mario = FindMario();
+        float dx = transform.position.x - mario.transform.position.x;
+        const float dist1 = 1.5f;
+        const float dist2 = 3;
+
+        if (dx > dist1 && _speed > -maxSpeed) {
+            _speed -= accFar * Time.fixedDeltaTime;
+        }
+        if (dx < -dist1 && _speed < maxSpeed) {
+            _speed += accFar * Time.fixedDeltaTime;
+        }
+        if (MathF.Abs(dx) < dist2) {
+            if (_speed < -lowSpeed && mario.Direction > 0) {
+                _speed += accNear * Time.fixedDeltaTime;
+            }
+            if (_speed > lowSpeed && mario.Direction < 0) {
+                _speed -= accNear * Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    private void Update() {
+        transform.Translate(_speed * Time.deltaTime, 0, 0);
+    }
+
+    private Mario FindMario() {
+        if (_cachedMario != null) {
+            return _cachedMario;
+        }
+        _cachedMario = FindObjectOfType<Mario>();
+        return _cachedMario;
+    }
+    
+    private float _speed = 0;
+    private Mario _cachedMario;
+}
+}
